ac356cd57054457207fbaaf9e41ebf70a75ac924
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2019-08-02 19:37:02 -0400

    Fix randomInt

--- loadTest.js
+++ loadTest.js
@@ -1,17 +1,14 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
-const randomInt = function(min, max) {
-  Math.random() * (max - min) + min;
-};
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
 export const options = {
-  vus: 1,
+  vus: 50,
   duration: '2m',
   rps: 1000
 };
 
 export default function() {
   http.get(`http://localhost:8888/${randomInt(9999000, 10000000)}/`);
-  // sleep(0.01); // configure to change RPS (currently at 100)
 };
