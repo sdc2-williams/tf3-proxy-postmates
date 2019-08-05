289c169c741f24086e60b3273d26342bd2d4ffba
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2019-08-05 14:59:12 -0400

    Prevent proxy from crashing when a request to a service times out

--- server/proxy.js
+++ server/proxy.js
@@ -5,10 +5,17 @@ const app = express();
 const proxy = httpProxy.createProxyServer();
 
 const PORT = process.env.PORT || 8888;
+app.use(express.static(__dirname + '/../public'));
 app.use('/:id', express.static(__dirname + '/../public'));
 
 app.all('/api/restaurant/:id', function(req, res) {
-  proxy.web(req, res, {target: 'http://ec2-100-24-63-53.compute-1.amazonaws.com'});
+  try {
+    proxy.web(req, res, {target: 'http://ec2-100-24-63-53.compute-1.amazonaws.com'});
+  } catch {
+    // This catches a 'socket hang up' error that proxy.web() throws if the module
+    // is too slow to respond.
+    res.status(504).end();
+  }
 });
 
 app.all('/api/menu/:id', function(req, res) {
