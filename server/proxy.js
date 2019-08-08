227617945bc46ad982aa491d1c069cf1a4e0f1cb
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2019-08-08 14:41:29 -0400

    Implement caching of service responses

--- server/proxy.js
+++ server/proxy.js
@@ -1,13 +1,15 @@
 require('newrelic');
 const express = require('express');
 const httpProxy = require('http-proxy');
+const NodeCache = require('node-cache');
+
 const app = express();
 const proxy = httpProxy.createProxyServer();
-
+const cache = new NodeCache();
 const PORT = process.env.PORT || 8888;
+
 app.use(express.static(__dirname + '/../public'));
 app.use('/:id', express.static(__dirname + '/../public'));
-
 
 const restaurantServiceUrls = [
   'http://52.90.230.178',
@@ -25,9 +27,32 @@ const getNextUrl = () => {
   return restaurantServiceUrls[urlIndex];
 };
 
+const cacheResponse = (proxyRes) => {
+  const chunks = [];
+  const id = proxyRes.client.parser.outgoing.socket._httpMessage. __NR_segment.transaction.nameState.pathStack[0].params.id;
+
+  proxyRes.on('data', chunk => {
+    chunks.push(chunk.toString());
+  })
+
+  proxyRes.on('end', () => {
+    const currentResponse = chunks.join('');
+    cache.set(id, currentResponse);
+  });
+};
+
+proxy.on('proxyRes', cacheResponse);
+
 app.all('/api/restaurant/:id', function(req, res) {
-  proxy.web(req, res, {target: getNextUrl()}, function(err) {
-    res.status(504).end();
+  const { id } = req.params;
+  cache.get(id, (err, value) => {
+    if (value) {
+      res.end(value);
+    } else {
+      proxy.web(req, res, {target: getNextUrl()}, function(err) {
+        res.status(504).end();
+      });
+    }
   });
 });
 
